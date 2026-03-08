f93ac7b95140a02b22d08f01768ac87fa6f0b7ca
chek1337 <DaniPlay1337@yandex.ru>
2026-03-08 20:33:19 +0000

    add qutebrowser

--- modules/programs/gui-tools/gui-browsers/qutebrowser.nix
+++ modules/programs/gui-tools/gui-browsers/qutebrowser.nix
@@ -4,6 +4,16 @@
     {
       programs.qutebrowser = {
         enable = true;
+        extraConfig = ''
+          c.statusbar.show = "in-mode"
+          c.tabs.padding = {"bottom": 5, "left": 16, "right": 16, "top": 5}
+          c.statusbar.padding = {"bottom": 4, "left": 16, "right": 16, "top": 4}
+          c.colors.webpage.preferred_color_scheme = "dark"
+          c.colors.webpage.darkmode.enabled = True
+          c.scrolling.smooth = True
+          config.bind("<Ctrl-d>", "cmd-run-with-count 15 scroll down")
+          config.bind("<Ctrl-u>", "cmd-run-with-count 15 scroll up")
+        '';
       };
     };
 }

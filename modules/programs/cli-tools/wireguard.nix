d0aece708545a26b5959d21e03f31d13d44921b7
chek1337 <DaniPlay1337@yandex.ru>
2026-03-09 00:45:45 +0000

    configure wireguard tunnel launching

--- modules/programs/cli-tools/wireguard.nix
+++ modules/programs/cli-tools/wireguard.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   flake.modules.nixos.wireguard =
-    { config, ... }:
+    { config, pkgs, ... }:
     {
       sops.secrets.wireguard = {
         sopsFile = inputs.self + "/secrets/wireguard.conf";
@@ -10,9 +10,56 @@
 
       networking.firewall.checkReversePath = "loose";
 
-      networking.wg-quick.interfaces.wg0 = {
-        configFile = config.sops.secrets.wireguard.path;
-        autostart = false;
+      boot.kernel.sysctl = {
+        "net.ipv4.ip_forward" = 1;
+      };
+
+      environment.systemPackages = with pkgs; [
+        wireguard-tools
+        iproute2
+      ];
+
+      systemd.services.netns-vpn = {
+        description = "VPN network namespace";
+        after = [ "network-online.target" ];
+        wants = [ "network-online.target" ];
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+          ExecStart = pkgs.writeShellScript "netns-vpn-start" ''
+            # Создать namespace
+            ${pkgs.iproute2}/bin/ip netns add vpn
+            ${pkgs.iproute2}/bin/ip -n vpn link set lo up
+
+            # Создать wg интерфейс и перенести в namespace
+            ${pkgs.iproute2}/bin/ip link add wg0 type wireguard
+            ${pkgs.iproute2}/bin/ip link set wg0 netns vpn
+
+            # Создать временный конфиг без Address/DNS
+            TMPCONF=$(mktemp)
+            grep -v "^Address\|^DNS" ${config.sops.secrets.wireguard.path} > $TMPCONF
+
+            # Настроить wireguard
+            ${pkgs.iproute2}/bin/ip netns exec vpn \
+              ${pkgs.wireguard-tools}/bin/wg setconf wg0 $TMPCONF
+            rm $TMPCONF
+
+            # Задать IP вручную
+            ${pkgs.iproute2}/bin/ip -n vpn addr add 10.0.0.2/32 dev wg0
+            ${pkgs.iproute2}/bin/ip -n vpn link set wg0 up
+            ${pkgs.iproute2}/bin/ip -n vpn route add default dev wg0
+
+            # DNS
+            mkdir -p /etc/netns/vpn
+            echo "nameserver 8.8.8.8" > /etc/netns/vpn/resolv.conf
+            chmod -R o+rX /etc/netns
+          '';
+          ExecStop = pkgs.writeShellScript "netns-vpn-stop" ''
+            ${pkgs.iproute2}/bin/ip netns pids vpn | xargs -r kill
+            ${pkgs.iproute2}/bin/ip netns del vpn 2>/dev/null || true
+            rm -rf /etc/netns/vpn
+          '';
+        };
       };
     };
 
@@ -20,8 +67,9 @@
     { ... }:
     {
       programs.zsh.shellAliases = {
-        wgu = "sudo systemctl start wg-quick-wg0.service";
-        wgd = "sudo systemctl stop wg-quick-wg0.service";
+        wgu = "sudo systemctl start netns-vpn.service";
+        wgd = "sudo systemctl stop netns-vpn.service";
+        vpn-run = "sudo ip netns exec vpn sudo -u $USER env WAYLAND_DISPLAY=$WAYLAND_DISPLAY XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR DISPLAY=$DISPLAY DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS";
       };
     };
 }

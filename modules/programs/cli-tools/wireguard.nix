367adb72e740cd283fc5d6eeea209acc19b1aaa8
chek1337 <DaniPlay1337@yandex.ru>
2026-03-09 03:26:10 +0000

    fix Windows-related settings (WSL)

--- modules/programs/cli-tools/wireguard.nix
+++ modules/programs/cli-tools/wireguard.nix
@@ -29,12 +29,12 @@
           ExecStart = pkgs.writeShellScript "netns-vpn-start" ''
             CONFFILE="${config.sops.secrets.wireguard.path}"
 
-            # Парсим Address и DNS из конфига
-            WG_ADDRESS=$(grep "^Address" "$CONFFILE" | head -1 | sed 's/.*= *//' | tr -d ' ')
-            WG_DNS=$(grep "^DNS" "$CONFFILE" | head -1 | sed 's/.*= *//' | tr -d ' ')
+            # Парсим Address и DNS из конфига (tr -d '\r' на случай Windows line endings)
+            WG_ADDRESS=$(grep "^Address" "$CONFFILE" | head -1 | sed 's/.*= *//' | tr -d ' \r')
+            WG_DNS=$(grep "^DNS" "$CONFFILE" | head -1 | sed 's/.*= *//' | tr -d ' \r')
 
-            echo "Using address: $WG_ADDRESS"
-            echo "Using DNS: $WG_DNS"
+            echo "Using address: '$WG_ADDRESS'"
+            echo "Using DNS: '$WG_DNS'"
 
             # Создать namespace
             ${pkgs.iproute2}/bin/ip netns add vpn
@@ -46,15 +46,15 @@
 
             # Создать временный конфиг без Address/DNS (wg не понимает эти поля)
             TMPCONF=$(mktemp)
-            grep -v "^Address\|^DNS" "$CONFFILE" > $TMPCONF
+            grep -v "^Address\|^DNS" "$CONFFILE" | tr -d '\r' > $TMPCONF
 
             # Настроить wireguard
             ${pkgs.iproute2}/bin/ip netns exec vpn \
-              ${pkgs.wireguard-tools}/bin/wg setconf wg0 $TMPCONF
+              ${pkgs.wireguard-tools}/bin/wg setconf wg0 $TMPCONF 2>/dev/null
             rm $TMPCONF
 
             # Задать IP из конфига
-            ${pkgs.iproute2}/bin/ip -n vpn addr add $WG_ADDRESS dev wg0
+            ${pkgs.iproute2}/bin/ip -n vpn addr add "$WG_ADDRESS" dev wg0 && echo "addr add OK" || echo "addr add FAILED"
             ${pkgs.iproute2}/bin/ip -n vpn link set wg0 up
             ${pkgs.iproute2}/bin/ip -n vpn route add default dev wg0
 
@@ -73,14 +73,29 @@
     };
 
   flake.modules.homeManager.wireguard =
-    { ... }:
+    { lib, ... }:
+    let
+      vpnEnv = lib.concatStringsSep " " [
+        "WAYLAND_DISPLAY=$WAYLAND_DISPLAY"
+        "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR"
+        "DISPLAY=$DISPLAY"
+        "DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS"
+        "QT_QPA_PLATFORMTHEME=$QT_QPA_PLATFORMTHEME"
+        "QT_STYLE_OVERRIDE=$QT_STYLE_OVERRIDE"
+        "XDG_CURRENT_DESKTOP=$XDG_CURRENT_DESKTOP"
+        "XDG_SESSION_TYPE=$XDG_SESSION_TYPE"
+        "XDG_DATA_DIRS=$XDG_DATA_DIRS"
+        "XDG_CONFIG_DIRS=$XDG_CONFIG_DIRS"
+        "QT_PLUGIN_PATH=$QT_PLUGIN_PATH"
+      ];
+    in
     {
       programs.zsh.shellAliases = {
         wgu = "sudo systemctl start netns-vpn.service";
         wgd = "sudo systemctl stop netns-vpn.service";
-        wgs = "sudo ip netns exec vpn wg show";
-        wgst = "sudo systemctl status netns-vpn.service";
-        vpn-run = "sudo ip netns exec vpn sudo -u $USER env WAYLAND_DISPLAY=$WAYLAND_DISPLAY XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR DISPLAY=$DISPLAY DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS";
+        wgs = "sudo systemctl status netns-vpn.service";
+        wgsh = "sudo ip netns exec vpn wg show";
+        vpn-run = "sudo ip netns exec vpn sudo -u $USER env ${vpnEnv}";
       };
     };
 }

233c60871d92286bdd25f94361a8c3eb6b33a72c
chek1337 <DaniPlay1337@yandex.ru>
2026-03-11 14:08:19 +0000

    add stylix support for WSL

--- modules/hosts/wsl-asuslaptop/default.nix
+++ modules/hosts/wsl-asuslaptop/default.nix
@@ -2,6 +2,7 @@
 let
   flakeConfig = config;
   shell = "zsh";
+  theme = "nord";
   modules = [
     "cli-tools"
     "python-dev"
@@ -13,12 +14,16 @@ in
   flake = {
     nixosConfigurations.wsl-asuslaptop = flakeConfig.flake.lib.mkSystems.wsl "wsl-asuslaptop";
     modules.nixos."hosts/wsl-asuslaptop" = {
-      imports = (flakeConfig.flake.lib.loadNixosAndHmModuleForUser flakeConfig modules) ++ [
-        flakeConfig.flake.modules.nixos.${shell}
-      ];
+      imports =
+        (flakeConfig.flake.lib.loadNixosAndHmModuleForUser flakeConfig modules)
+        ++ [ flakeConfig.flake.modules.nixos.${theme} ]
+        ++ [ flakeConfig.flake.modules.nixos.${shell} ];
     };
     modules.homeManager."hosts/wsl-asuslaptop" = {
-      imports = [ flakeConfig.flake.modules.homeManager.${shell} ];
+      imports = [
+        flakeConfig.flake.modules.homeManager.${theme}
+        flakeConfig.flake.modules.homeManager.${shell}
+      ];
     };
   };
 }

5f11bfb06a5a7a2780da415272a2049ef644d8f4
chek1337 <DaniPlay1337@yandex.ru>
2026-03-18 22:51:56 +0700

    perf: add parallel build settings and nix-community cachix

--- modules/flake/nixos-classes/nixos.nix
+++ modules/flake/nixos-classes/nixos.nix
@@ -11,19 +11,23 @@
         inputs.hyprland.nixosModules.default
       ];
       nix.settings = {
+        max-jobs = "auto";
+        cores = 0;
         substituters = [
           "https://cache.nixos.org"
           "https://hyprland.cachix.org"
+          "https://nix-community.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+        experimental-features = [
+          "nix-command"
+          "flakes"
         ];
       };
-      nix.settings.experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
       nixpkgs.config.allowUnfree = true;
       nixpkgs.config.permittedInsecurePackages = [ "openssl-1.1.1w" ];
       home-manager.backupFileExtension = "backup";

9a806a99bd4a833b6c48aed319c085b4d8f4fa27
chek1337 <DaniPlay1337@yandex.ru>
2026-03-19 13:40:08 +0700

    feat: add ISO build configuration for desktop-home host

--- modules/hosts/desktop-home/default.nix
+++ modules/hosts/desktop-home/default.nix
@@ -29,6 +29,7 @@ in
 {
   flake = {
     nixosConfigurations.${hostname} = flakeConfig.flake.lib.mkSystems.linux username hostname;
+    nixosConfigurations."iso-${hostname}" = flakeConfig.flake.lib.mkSystems.iso username hostname;
     modules.nixos."hosts/${hostname}" = {
       imports = (flakeConfig.flake.lib.loadNixosAndHmModuleForUser flakeConfig modules) ++ [
         ./_hardware-configuration.nix

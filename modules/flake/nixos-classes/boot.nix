89680f50d24117e7ec551c024daed99e8c8efacc
chek1337 <DaniPlay1337@yandex.ru>
2026-03-24 19:59:16 +0700

    revert: remove boot menu generation limit

--- modules/flake/nixos-classes/boot.nix
+++ modules/flake/nixos-classes/boot.nix
@@ -7,7 +7,6 @@
         # kernelPackages = pkgs.linuxPackages_latest;
         loader = {
           systemd-boot.enable = true;
-          systemd-boot.configurationLimit = 5;
           systemd-boot.sortKey = "z-nixos";
           efi.canTouchEfiVariables = true;
         };

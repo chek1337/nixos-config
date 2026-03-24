0722acd7af36772c2671bd39798f1f65ecfa3121
chek1337 <DaniPlay1337@yandex.ru>
2026-03-24 20:21:53 +0700

    revert: remove boot menu sorting customizations

--- modules/flake/nixos-classes/boot.nix
+++ modules/flake/nixos-classes/boot.nix
@@ -7,12 +7,6 @@
         # kernelPackages = pkgs.linuxPackages_latest;
         loader = {
           systemd-boot.enable = true;
-          systemd-boot.extraEntries = {
-            "00-windows.conf" = ''
-              title Windows
-              efi /EFI/Microsoft/Boot/bootmgfw.efi
-            '';
-          };
           efi.canTouchEfiVariables = true;
         };
       };

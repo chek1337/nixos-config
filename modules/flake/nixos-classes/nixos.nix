83764093c0115954fd25659016fc7dea707ca63f
chek1337 <DaniPlay1337@yandex.ru>
2026-03-25 03:59:21 +0700

    chore: comment out zen-browser cachix substituter

--- modules/flake/nixos-classes/nixos.nix
+++ modules/flake/nixos-classes/nixos.nix
@@ -15,12 +15,12 @@
         substituters = [
           "https://cache.nixos.org"
           "https://nix-community.cachix.org"
-          "https://zen-browser.cachix.org"
+          # "https://zen-browser.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          "zen-browser.cachix.org-1:z/QLGrEkiBYF/7zoHX1Hpuv0B26QrmbVBSy9yDD2tSs="
+          # "zen-browser.cachix.org-1:z/QLGrEkiBYF/7zoHX1Hpuv0B26QrmbVBSy9yDD2tSs="
         ];
         keep-outputs = true;
         keep-derivations = true;

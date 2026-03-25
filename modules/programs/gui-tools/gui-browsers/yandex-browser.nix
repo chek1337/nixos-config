1d555b5901eb00f495ebef0584317cc0334a7cae
chek1337 <DaniPlay1337@yandex.ru>
2026-03-26 01:31:11 +0700

    fix: remove stale icon-theme.cache from yandex-browser package
    The package generates icon-theme.cache in postFixup via
    gtk-update-icon-cache, but this cache only contains yandex-browser
    icons. When merged into the Nix profile, it shadows all other app
    icons in hicolor, causing missing icons in Noctalia's app launcher.

--- modules/programs/gui-tools/gui-browsers/yandex-browser.nix
+++ modules/programs/gui-tools/gui-browsers/yandex-browser.nix
@@ -4,7 +4,11 @@
     { ... }:
     {
       home.packages = [
-        inputs.yandex-browser.packages.x86_64-linux.yandex-browser-stable
+        (inputs.yandex-browser.packages.x86_64-linux.yandex-browser-stable.overrideAttrs (old: {
+          postFixup = (old.postFixup or "") + ''
+            rm -f $out/share/icons/hicolor/icon-theme.cache
+          '';
+        }))
       ];
     };
 }

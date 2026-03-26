5b4038cb5456ebea61e1559e39159c531a8d3012
chek1337 <DaniPlay1337@yandex.ru>
2026-03-26 22:35:53 +0700

    feat: open text files in nvim via Nautilus using terminal .desktop entry

--- modules/programs/gui-tools/nautilus.nix
+++ modules/programs/gui-tools/nautilus.nix
@@ -1,14 +1,37 @@
 {
   flake.modules.homeManager.nautilus =
-    { pkgs, ... }:
+    { pkgs, config, ... }:
+    let
+      terminal = config.home.sessionVariables.TERMINAL or "kitty";
+    in
     {
       home.packages = with pkgs; [ nautilus ];
+
+      xdg.desktopEntries.nvim-terminal = {
+        name = "Neovim";
+        exec = "${terminal} -e nvim %F";
+        icon = "nvim";
+        terminal = false;
+        mimeType = [
+          "text/plain"
+          "text/x-script"
+          "application/x-shellscript"
+          "text/x-c"
+          "text/x-python"
+          "text/x-nix"
+        ];
+        categories = [
+          "Utility"
+          "TextEditor"
+        ];
+      };
 
       xdg.mimeApps.defaultApplications = {
         "inode/directory" = "org.gnome.Nautilus.desktop";
         "audio/*" = "mpv.desktop";
         "video/*" = "mpv.desktop";
         "image/*" = "imv.desktop";
+        "text/plain" = "nvim-terminal.desktop";
       };
     };
 }

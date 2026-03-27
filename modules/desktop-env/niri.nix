f2dbe096d7a667396b17316a2957e9703c908f4e
chek1337 <DaniPlay1337@yandex.ru>
2026-03-27 23:31:47 +0700

    refactor: remove niri-float-sticky daemon

--- modules/desktop-env/niri.nix
+++ modules/desktop-env/niri.nix
@@ -29,17 +29,17 @@
     let
       cfg = config.services.niri;
 
-      niri-float-sticky = pkgs.buildGoModule {
-        pname = "niri-float-sticky";
-        version = "0.0.8";
-        src = pkgs.fetchFromGitHub {
-          owner = "probeldev";
-          repo = "niri-float-sticky";
-          rev = "v0.0.8";
-          hash = "sha256-iNd10SZgO+DY+VSqTfDYx19SU2styiG7AC+yLwb9yj8=";
-        };
-        vendorHash = "sha256-GqbY3qkPjMxyW9RTsN9hkgM3Bda6A8rb2kR4YQW1nFI=";
-      };
+      # niri-float-sticky = pkgs.buildGoModule {
+      #   pname = "niri-float-sticky";
+      #   version = "0.0.8";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "probeldev";
+      #     repo = "niri-float-sticky";
+      #     rev = "v0.0.8";
+      #     hash = "sha256-iNd10SZgO+DY+VSqTfDYx19SU2styiG7AC+yLwb9yj8=";
+      #   };
+      #   vendorHash = "sha256-GqbY3qkPjMxyW9RTsN9hkgM3Bda6A8rb2kR4YQW1nFI=";
+      # };
 
       mkOutputBlock =
         name: out:
@@ -275,7 +275,6 @@
         }
 
         spawn-at-startup "xwayland-satellite"
-        spawn-at-startup "${niri-float-sticky}/bin/niri-float-sticky"
 
         window-rule {
             match app-id=r#"firefox$"# title="^Picture-in-Picture$"

eec9625c1266445565aa525f480ef83ea6ce0d2a
chek1337 <DaniPlay1337@yandex.ru>
2026-03-30 00:48:35 +0700

    feat: add 13 gh extensions via activation script
    Install gh extensions (dash, s, f, eco, notify, poi, markdown-preview,
    grep, pr-review, branch, cp, download, user-stars) using a home-manager
    activation script to support extensions not packaged in nixpkgs.

--- modules/programs/cli-tools/gh.nix
+++ modules/programs/cli-tools/gh.nix
@@ -1,6 +1,6 @@
 {
   flake.modules.homeManager.gh =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
     {
       programs.gh = {
         enable = true;
@@ -9,5 +9,40 @@
           prompt = "enabled";
         };
       };
+
+      home.activation.installGhExtensions = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        export PATH="${
+          lib.makeBinPath [
+            pkgs.gh
+            pkgs.git
+            pkgs.openssh
+            pkgs.coreutils
+          ]
+        }:$PATH"
+        export GH_PROTOCOL=https
+
+        extensions=(
+          "dlvhdr/gh-dash"
+          "gennaro-tedesco/gh-s"
+          "gennaro-tedesco/gh-f"
+          "jrnxf/gh-eco"
+          "meiji163/gh-notify"
+          "seachicken/gh-poi"
+          "yusukebe/gh-markdown-preview"
+          "k1LoW/gh-grep"
+          "agynio/gh-pr-review"
+          "mislav/gh-branch"
+          "mislav/gh-cp"
+          "yuler/gh-download"
+          "korosuke613/gh-user-stars"
+        )
+
+        for ext in "''${extensions[@]}"; do
+          name="''${ext#*/}"
+          if [ ! -d "$HOME/.local/share/gh/extensions/$name" ]; then
+            run gh extension install "$ext" || true
+          fi
+        done
+      '';
     };
 }

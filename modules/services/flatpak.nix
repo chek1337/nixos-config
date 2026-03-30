4376e3ada478aebd050a8504e0e42ab0a90870f1
chek1337 <DaniPlay1337@yandex.ru>
2026-03-30 21:46:12 +0700

    refactor: consolidate flatpak config into nixos module
    Move remotes configuration from homeManager module to nixos module
    and remove the now-unnecessary homeManager flatpak module.

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -3,17 +3,15 @@
   flake.modules.nixos.flatpak = {
     imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
 
-    services.flatpak.enable = true;
-  };
+    services.flatpak = {
+      enable = true;
 
-  flake.modules.homeManager.flatpak = {
-    imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
-
-    services.flatpak.remotes = [
-      {
-        name = "flathub";
-        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      }
-    ];
+      remotes = [
+        {
+          name = "flathub";
+          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+        }
+      ];
+    };
   };
 }

2884c499a4717c970c403da72b9cf8626c0b231f
chek1337 <DaniPlay1337@yandex.ru>
2026-04-08 22:26:22 +0700

    feat(qutebrowser): add Russian keyboard layout key mappings
    Maps Cyrillic keys to their Latin equivalents so qutebrowser shortcuts work regardless of input language.

--- modules/programs/gui-tools/gui-browsers/qutebrowser.nix
+++ modules/programs/gui-tools/gui-browsers/qutebrowser.nix
@@ -5,7 +5,10 @@
       programs.qutebrowser = {
         enable = true;
         quickmarks = lib.listToAttrs (
-          map (b: { name = b.name; value = b.url; }) config.browserBookmarks
+          map (b: {
+            name = b.name;
+            value = b.url;
+          }) config.browserBookmarks
         );
         searchEngines = {
           DEFAULT = "https://duckduckgo.com/?q={}";
@@ -41,6 +44,15 @@
           config.bind(",A", "hint links spawn umpv-mini {hint-url}")
           config.bind(";A", "hint --rapid links spawn umpv-mini {hint-url}")
           c.hints.uppercase = True
+
+          c.bindings.key_mappings.update({
+              'й': 'q', 'ц': 'w', 'у': 'e', 'к': 'r', 'е': 't', 'н': 'y', 'г': 'u', 'ш': 'i', 'щ': 'o', 'з': 'p', 'х': '[', 'ъ': ']',
+              'ф': 'a', 'ы': 's', 'в': 'd', 'а': 'f', 'п': 'g', 'р': 'h', 'о': 'j', 'л': 'k', 'д': 'l', 'ж': ';', 'э': "'",
+              'я': 'z', 'ч': 'x', 'с': 'c', 'м': 'v', 'и': 'b', 'т': 'n', 'ь': 'm', 'б': ',', 'ю': '.',
+              'Й': 'Q', 'Ц': 'W', 'У': 'E', 'К': 'R', 'Е': 'T', 'Н': 'Y', 'Г': 'U', 'Ш': 'I', 'Щ': 'O', 'З': 'P', 'Х': '{', 'Ъ': '}',
+              'Ф': 'A', 'Ы': 'S', 'В': 'D', 'А': 'F', 'П': 'G', 'Р': 'H', 'О': 'J', 'Л': 'K', 'Д': 'L', 'Ж': ':', 'Э': '"',
+              'Я': 'Z', 'Ч': 'X', 'С': 'C', 'М': 'V', 'И': 'B', 'Т': 'N', 'Ь': 'M', 'Б': '<', 'Ю': '>',
+          })
         '';
       };
     };

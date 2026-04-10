e3087557f4bd8e3ecb498ab0f4c8f8d31d67ddfc
chek1337 <DaniPlay1337@yandex.ru>
2026-04-10 19:30:38 +0700

    feat(niri): add hotkey for notifications

--- modules/desktop-env/niri.nix
+++ modules/desktop-env/niri.nix
@@ -306,6 +306,7 @@ in
             Mod+V       { toggle-window-floating; }
             Mod+Alt+V { switch-focus-between-floating-and-tiling; }
             Mod+W       { toggle-column-tabbed-display; }
+            Mod+N { spawn "noctalia-shell" "ipc" "call" "notifications" "invokeDefaultAndDismiss"; }
             Mod+P repeat=false { toggle-window-pinned; }
 
             Print      { spawn-sh "grim -t ppm - | satty --filename - --copy-command=wl-copy --initial-tool=crop --output-filename=\"~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png\" --actions-on-escape=\"save-to-clipboard,exit\""; }

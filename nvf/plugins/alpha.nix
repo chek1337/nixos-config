1569ff94cba3ba229591100c11a5d034da45ae4e
chek1337 <DaniPlay1337@yandex.ru>
2026-04-14 05:03:07 +0700

    feat: rewrite alpha.nix using nvf declarative dashboard API
    Replace commented-out raw Lua with nvf's vim.dashboard.alpha layout DSL.

--- nvf/plugins/alpha.nix
+++ nvf/plugins/alpha.nix
@@ -1,158 +1,149 @@
-{ stylixColors, ... }:
+{ lib, ... }:
 let
-  c = stylixColors;
+  lua = lib.mkLuaInline;
+  keymap = key: command: [
+    "n"
+    "${key}"
+    "<cmd>${command}<cr>"
+    {
+      noremap = true;
+      silent = true;
+      nowait = true;
+    }
+  ];
 in
 {
-  #  vim.startPlugins = [ "alpha-nvim" ];
-  #  vim.visuals.nvim-web-devicons.enable = true;
-  #
-  #  vim.pluginRC.alpha = ''
-  #    local alpha     = require('alpha')
-  #    local dashboard = require('alpha.themes.dashboard')
-  #    local utils     = require('alpha.utils')
-  #
-  #    local function getGreeting(name)
-  #      local t  = os.date('*t')
-  #      local dt = os.date(' %Y-%m-%d-%A   %H:%M:%S ')
-  #      local greetings = {
-  #        [1] = '  Sleep well',
-  #        [2] = '  Good morning',
-  #        [3] = '  Good afternoon',
-  #        [4] = '  Good evening',
-  #        [5] = '󰖔  Good night',
-  #      }
-  #      local idx
-  #      if t.hour == 23 or t.hour < 7 then idx = 1
-  #      elseif t.hour < 12               then idx = 2
-  #      elseif t.hour < 18               then idx = 3
-  #      elseif t.hour < 21               then idx = 4
-  #      else                                  idx = 5 end
-  #      return dt .. '  ' .. greetings[idx] .. ', ' .. name
-  #    end
-  #
-  #    local fill = vim.fn.winheight(0) - 43
-  #    local logo = (fill >= 0 and [[
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #
-  #    ]] or "") ..
-  #        [[
-  #                                          
-  #       ███████████           █████      ██
-  #      ███████████             █████ 
-  #      ████████████████ ███████████ ███   ███████
-  #     ████████████████ ████████████ █████ ██████████████
-  #    █████████████████████████████ █████ █████ ████ █████
-  #  ██████████████████████████████████ █████ █████ ████ █████
-  # ██████  ███ █████████████████ ████ █████ █████ ████ ██████
-  # ██████   ██  ███████████████   ██ █████████████████
-  #
-  #      ]]
-  #
-  #    -- Stylix base16 highlight groups
-  #    vim.api.nvim_set_hl(0, 'AlphaHdr_Orange', { fg = '#${c.base09}' })
-  #    vim.api.nvim_set_hl(0, 'AlphaHdr_Yellow', { fg = '#${c.base0A}' })
-  #    vim.api.nvim_set_hl(0, 'AlphaHdr_Shadow', { fg = '#${c.base01}' })
-  #    vim.api.nvim_set_hl(0, 'AlphaHdr_Green',  { fg = '#${c.base0B}' })
-  #    vim.api.nvim_set_hl(0, 'AlphaHdr_Cyan',   { fg = '#${c.base0C}' })
-  #
-  #    local header_hl = {}
-  #
-  #    if fill >= 0 then
-  #      for _ = 1, fill do table.insert(header_hl, {}) end
-  #    end
-  #    table.insert(header_hl, {})                                          -- leading empty line
-  #    table.insert(header_hl, { { 'AlphaHdr_Cyan',   46, 48 } })          -- space line
-  #    table.insert(header_hl, {                                            -- ███████████ … ██
-  #      { 'AlphaHdr_Orange', 7,  22 },
-  #      { 'AlphaHdr_Green',  33, 40 },
-  #      { 'AlphaHdr_Cyan',   40, 50 },
-  #    })
-  #    table.insert(header_hl, {                                            -- ███████████ … █████
-  #      { 'AlphaHdr_Orange', 6,  21 },
-  #      { 'AlphaHdr_Green',  33, 45 },
-  #    })
-  #    table.insert(header_hl, {                                            -- ████████████████ … ███████
-  #      { 'AlphaHdr_Orange', 6,  19 },
-  #      { 'AlphaHdr_Shadow', 19, 20 },
-  #      { 'AlphaHdr_Yellow', 20, 35 },
-  #      { 'AlphaHdr_Green',  35, 45 },
-  #      { 'AlphaHdr_Cyan',   45, 90 },
-  #    })
-  #    table.insert(header_hl, {                                            -- ████████████████ … ██████████████
-  #      { 'AlphaHdr_Orange', 5,  18 },
-  #      { 'AlphaHdr_Yellow', 18, 36 },
-  #      { 'AlphaHdr_Green',  36, 45 },
-  #      { 'AlphaHdr_Cyan',   45, 90 },
-  #    })
-  #    table.insert(header_hl, {                                            -- █████████████████████████████ …
-  #      { 'AlphaHdr_Orange', 4,  17 },
-  #      { 'AlphaHdr_Yellow', 17, 24 },
-  #      { 'AlphaHdr_Shadow', 24, 28 },
-  #      { 'AlphaHdr_Yellow', 28, 37 },
-  #      { 'AlphaHdr_Green',  37, 46 },
-  #      { 'AlphaHdr_Cyan',   46, 90 },
-  #    })
-  #    table.insert(header_hl, {                                            -- ██████████████████████████████████ …
-  #      { 'AlphaHdr_Orange', 2,  17 },
-  #      { 'AlphaHdr_Yellow', 17, 38 },
-  #      { 'AlphaHdr_Green',  38, 45 },
-  #      { 'AlphaHdr_Cyan',   46, 90 },
-  #    })
-  #    table.insert(header_hl, {                                            -- ██████  ███ …
-  #      { 'AlphaHdr_Orange', 1,  17 },
-  #      { 'AlphaHdr_Yellow', 17, 38 },
-  #      { 'AlphaHdr_Green',  38, 45 },
-  #      { 'AlphaHdr_Cyan',   46, 90 },
-  #    })
-  #    table.insert(header_hl, {                                            -- ██████   ██ …
-  #      { 'AlphaHdr_Shadow', 1,  37 },
-  #      { 'AlphaHdr_Green',  37, 91 },
-  #    })
-  #
-  #    local header_val = vim.split(logo, '\n')
-  #    header_hl = utils.charhl_to_bytehl(header_hl, header_val, false)
-  #
-  #    dashboard.section.header.opts.hl = header_hl
-  #    dashboard.section.header.val     = header_val
-  #
-  #    dashboard.section.buttons.val = {
-  #      dashboard.button('n', '  New file',     ':ene <BAR> startinsert<CR>'),
-  #      dashboard.button('f', '  Find file',    '<cmd>lua require("snacks").picker.files()<CR>'),
-  #      dashboard.button('r', '󰄉  Recent files', '<cmd>lua require("snacks").picker.recent()<CR>'),
-  #      dashboard.button('g', '  Live grep',    '<cmd>lua require("snacks").picker.grep()<CR>'),
-  #      dashboard.button('s', '  Config',       '<cmd>lua require("snacks").picker.files({ cwd = vim.fn.stdpath("config") })<CR>'),
-  #      dashboard.button('q', '󰿅  Quit',         '<cmd>q<CR>'),
-  #    }
-  #    dashboard.section.buttons.opts.hl = 'AlphaHdr_Orange'
-  #
-  #    local userName = vim.fn.expand('$USER')
-  #    dashboard.section.footer.val = { "", getGreeting(userName) }
-  #
-  #    local group = vim.api.nvim_create_augroup('AlphaDashboard', { clear = true })
-  #    vim.api.nvim_create_autocmd('User', {
-  #      group = group, pattern = 'AlphaReady',
-  #      callback = function()
-  #        vim.opt.showcmd = false
-  #        vim.opt.ruler   = false
-  #      end,
-  #    })
-  #    vim.api.nvim_create_autocmd('BufUnload', {
-  #      group = group, pattern = '<buffer>',
-  #      callback = function()
-  #        vim.opt.showcmd = true
-  #        vim.opt.ruler   = true
-  #      end,
-  #    })
-  #
-  #    dashboard.opts.opts.noautocmd = true
-  #    alpha.setup(dashboard.opts)
-  #  '';
+  vim.visuals.nvim-web-devicons.enable = true;
+
+  vim.dashboard.alpha = {
+    enable = true;
+    theme = null;
+    layout = [
+      {
+        type = "padding";
+        val = 15;
+      }
+
+      {
+        type = "text";
+        val = [
+          "      ████ ██████           █████      ██                     "
+          "     ███████████             █████                             "
+          "     █████████ ███████████████████ ███   ███████████    "
+          "    █████████  ███    █████████████ █████ ██████████████    "
+          "   █████████ ██████████ █████████ █████ █████ ████ █████    "
+          " ███████████ ███    ███ █████████ █████ █████ ████ █████  "
+          "██████  █████████████████████ ████ █████ █████ ████ ██████ "
+        ];
+        opts = {
+          position = "center";
+          hl = "Type";
+        };
+      }
+
+      {
+        type = "padding";
+        val = 2;
+      }
+
+      {
+        type = "group";
+        val = [
+          {
+            type = "button";
+            val = "  New File";
+            on_press = ":ene | startinsert";
+            opts = {
+              position = "center";
+              shortcut = "n";
+              keymap = keymap "n" "ene | startinsert";
+              cursor = 3;
+              width = 50;
+              align_shortcut = "right";
+              hl_shortcut = "Keyword";
+            };
+          }
+          {
+            type = "button";
+            val = "  Find File";
+            on_press = lua ''function() require("snacks").picker.files() end'';
+            opts = {
+              position = "center";
+              shortcut = "f";
+              keymap = keymap "f" "lua require('snacks').picker.files()";
+              cursor = 3;
+              width = 50;
+              align_shortcut = "right";
+              hl_shortcut = "Keyword";
+            };
+          }
+          {
+            type = "button";
+            val = "   Find Text";
+            on_press = lua ''function() require("snacks").picker.grep() end'';
+            opts = {
+              position = "center";
+              shortcut = "g";
+              keymap = keymap "g" "lua require('snacks').picker.grep()";
+              cursor = 3;
+              width = 50;
+              align_shortcut = "right";
+              hl_shortcut = "Keyword";
+            };
+          }
+          {
+            type = "button";
+            val = "  Recent Files";
+            on_press = lua ''function() require("snacks").picker.recent() end'';
+            opts = {
+              position = "center";
+              shortcut = "r";
+              keymap = keymap "r" "lua require('snacks').picker.recent()";
+              cursor = 3;
+              width = 50;
+              align_shortcut = "right";
+              hl_shortcut = "Keyword";
+            };
+          }
+          # {
+          #   type = "button";
+          #   val = "  Config";
+          #   on_press = lua ''
+          #     function()
+          #       require("snacks").picker.files({ cwd = vim.fn.stdpath("config") })
+          #     end
+          #   '';
+          #   opts = {
+          #     position = "center";
+          #     shortcut = "c";
+          #     keymap = keymap "c" "lua require('snacks').picker.files({ cwd = vim.fn.stdpath('config') })";
+          #     cursor = 3;
+          #     width = 50;
+          #     align_shortcut = "right";
+          #     hl_shortcut = "Keyword";
+          #   };
+          # }
+          {
+            type = "button";
+            val = "  Quit";
+            on_press = ":qa";
+            opts = {
+              position = "center";
+              shortcut = "q";
+              keymap = keymap "q" "qa";
+              cursor = 3;
+              width = 50;
+              align_shortcut = "right";
+              hl_shortcut = "Keyword";
+            };
+          }
+        ];
+        opts = {
+          spacing = 1;
+          position = "center";
+          hl = "Function";
+        };
+      }
+    ];
+  };
 }

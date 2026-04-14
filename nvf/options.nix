bbab846c64351c9e1e8fcbc9fe2290391ad45193
chek1337 <DaniPlay1337@yandex.ru>
2026-04-14 22:57:41 +0700

    feat(nvf): add yank highlight

--- nvf/options.nix
+++ nvf/options.nix
@@ -30,13 +30,25 @@
       maplocalleader = " ";
     };
 
-    luaConfigRC.autoread-checktime = ''
-      vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI" }, {
-        group = vim.api.nvim_create_augroup("AutoReadOnChange", { clear = true }),
-        callback = function()
-          vim.cmd("silent! checktime")
-        end,
-      })
-    '';
+    luaConfigRC.autoread-checktime = # lua
+      ''
+        vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI" }, {
+          group = vim.api.nvim_create_augroup("AutoReadOnChange", { clear = true }),
+          callback = function()
+            vim.cmd("silent! checktime")
+          end,
+        })
+      '';
+
+    luaConfigRC.highlightOnYank = # lua
+      ''
+        vim.api.nvim_create_autocmd("TextYankPost", {
+          group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+          callback = function()
+            (vim.hl or vim.highlight).on_yank()
+          end,
+        })
+      '';
+
   };
 }

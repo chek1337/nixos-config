4a909f8dd02d4ba054081c119673a83354d31de4
chek1337 <DaniPlay1337@yandex.ru>
2026-04-17 05:30:55 +0700

    feat(nvf): extend which-key groups for trouble, tabs, and navigation
    Add missing prefix labels: x (Diagnostics), b (Buffers), <tab> (Tabs),
    sn (Notifications), sr (Replace), ] (Next), [ (Prev)

--- nvf/plugins/qol/which-key.nix
+++ nvf/plugins/qol/which-key.nix
@@ -13,12 +13,20 @@
       "<leader>u" = "UI";
       "<leader>f" = "File";
       "<leader>C" = "Color";
+      "<leader>b" = "Buffers";
+      "<leader>x" = "Diagnostics";
+      "<leader><tab>" = "Tabs";
+
       "<leader>gh" = "Hunks";
       "<leader>dp" = "Print";
-
       "<leader>gd" = "Diff";
       "<leader>gdh" = "History";
       "<leader>S" = "Session";
+      "<leader>sn" = "Notifications";
+      "<leader>sr" = "Replace";
+
+      "]" = "Next";
+      "[" = "Prev";
     };
   };
 }
--- nvf/plugins/ui/visuals.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  vim.visuals = {
-    # nvim-web-devicons.enable = true;
-    # fidget-nvim.enable = true;
-    # indent-blankline.enable = true;
-    # highlight-undo.enable = true;
-  };
-}

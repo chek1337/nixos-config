22a2931a0095a9b1e1a746c2417bf9ee96ffa1dc
chek1337 <DaniPlay1337@yandex.ru>
2026-04-20 20:00:09 +0700

    feat(vscode): add extensions for VsCode

--- modules/programs/gui-tools/gui-code-editors/vscode.nix
+++ modules/programs/gui-tools/gui-code-editors/vscode.nix
@@ -5,6 +5,23 @@
       programs.vscode = {
         enable = true;
         package = pkgs.vscode;
+        profiles.default.extensions =
+          with pkgs.vscode-extensions;
+          [
+            llvm-vs-code-extensions.vscode-clangd
+            ms-vscode.cmake-tools
+            ms-vscode-remote.remote-containers
+            ms-azuretools.vscode-docker
+            twxs.cmake
+          ]
+          ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+              name = "yocto-bitbake";
+              publisher = "yocto-project";
+              version = "2.1.0";
+              sha256 = "sha256-X7igyJb5NSZbxxOMQS+bd052tXU7pUfCc2CRGRHusBQ=";
+            }
+          ];
       };
     };
 }

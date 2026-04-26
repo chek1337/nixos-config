d67950e44b3868721f6e4550ac28623ee9d94329
chek1337 <DaniPlay1337@yandex.ru>
2026-04-26 22:43:39 +0700

    feat: comment cashes

--- modules/flake/nixos-classes/nixos.nix
+++ modules/flake/nixos-classes/nixos.nix
@@ -11,15 +11,15 @@
         cores = 0;
         substituters = [
           "https://cache.nixos.org"
-          "https://nix-community.cachix.org"
+          # "https://nix-community.cachix.org"
           # "https://zen-browser.cachix.org"
-          "https://yandex-browser-nix.cachix.org"
+          # "https://yandex-browser-nix.cachix.org"
         ];
         trusted-public-keys = [
           "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           # "zen-browser.cachix.org-1:z/QLGrEkiBYF/7zoHX1Hpuv0B26QrmbVBSy9yDD2tSs="
-          "yandex-browser-nix.cachix.org-1:KTUynR1mK6m4ZVPgM2U5cb/yTa9vBGMU+eRY/l/b7vw="
+          # "yandex-browser-nix.cachix.org-1:KTUynR1mK6m4ZVPgM2U5cb/yTa9vBGMU+eRY/l/b7vw="
         ];
         connect-timeout = 3;
         keep-outputs = true;

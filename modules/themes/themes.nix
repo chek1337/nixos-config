c0449ee8b437cdd57f4e6897303b3a2c4d140796
chek1337 <DaniPlay1337@yandex.ru>
2026-05-02 16:18:36 +0700

    feat: change font to Hack Nerd Font

--- modules/themes/themes.nix
+++ modules/themes/themes.nix
@@ -30,8 +30,8 @@ let
       image = scheme.image;
       fonts = {
         monospace = {
-          package = pkgs.nerd-fonts.fira-code;
-          name = "FiraCode Nerd Font";
+          package = pkgs.nerd-fonts.hack;
+          name = "Hack Nerd Font";
         };
         sansSerif = {
           package = pkgs.inter;

fe21d640b32da310c07d8118d8d7fb18449340c6
chek1337 <DaniPlay1337@yandex.ru>
2026-05-12 23:41:46 +0700

    feat(nvf): port LazyVim gitsigns mappings and close-with-q autocmd
    - gitsigns: replace nvf's global mappings with a LazyVim-parity
      on_attach (buffer-local maps, nav_hunk API, preview_hunk_inline,
      blame buffer, first/last hunk, `ih` text object)
    - autocmds: add close_with_q for help/qf/gitsigns-blame/etc. so `q`
      closes those windows like in LazyVim; supersedes qf_unlisted
    - cli-tools: re-enable lazyvim alongside nvf

--- nvf/options.nix
+++ nvf/options.nix
@@ -66,13 +66,40 @@
         })
       '';
 
-    luaConfigRC.qf-unlisted = # lua
+    luaConfigRC.close-with-q = # lua
       ''
         vim.api.nvim_create_autocmd("FileType", {
-          group = vim.api.nvim_create_augroup("qf_unlisted", { clear = true }),
-          pattern = "qf",
-          callback = function(args)
-            vim.bo[args.buf].buflisted = false
+          group = vim.api.nvim_create_augroup("close_with_q", { clear = true }),
+          pattern = {
+            "PlenaryTestPopup",
+            "checkhealth",
+            "dap-float",
+            "dbout",
+            "gitsigns-blame",
+            "grug-far",
+            "help",
+            "lspinfo",
+            "neotest-output",
+            "neotest-output-panel",
+            "neotest-summary",
+            "notify",
+            "qf",
+            "spectre_panel",
+            "startuptime",
+            "tsplayground",
+          },
+          callback = function(event)
+            vim.bo[event.buf].buflisted = false
+            vim.schedule(function()
+              vim.keymap.set("n", "q", function()
+                vim.cmd("close")
+                pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+              end, {
+                buffer = event.buf,
+                silent = true,
+                desc = "Quit buffer",
+              })
+            end)
           end,
         })
       '';
--- nvf/plugins/git/gitsigns.nix
+++ nvf/plugins/git/gitsigns.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ ... }:
 {
   vim.git = {
     enable = true;
@@ -22,15 +22,6 @@
           topdelete.text = "";
           changedelete.text = "▎";
         };
-        on_attach =
-          lib.mkLuaInline # lua
-            ''
-              function(buffer)
-                vim.keymap.set("n", "<leader>ghp",
-                  package.loaded.gitsigns.preview_hunk_inline,
-                  { buffer = buffer, desc = "Preview Hunk Inline [Gitsigns]", silent = true })
-              end
-            '';
       };
     };
   };

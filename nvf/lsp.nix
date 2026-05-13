cf2e59dde306f50a0bbe3d1535e22a1980a285ea
chek1337 <DaniPlay1337@yandex.ru>
2026-05-13 18:28:47 +0700

    feat(nvf): add clangd source/header switch keymap on LspAttach

--- nvf/lsp.nix
+++ nvf/lsp.nix
@@ -17,6 +17,19 @@
         pcall(vim.keymap.del, "n", lhs)
         pcall(vim.keymap.del, "x", lhs)
       end
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("clangd_switch_source_header", { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and client.name == "clangd" then
+            vim.keymap.set("n", "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", {
+              buffer = args.buf,
+              desc = "Switch Source/Header (C/C++)",
+            })
+          end
+        end,
+      })
     '';
 
     lsp = {

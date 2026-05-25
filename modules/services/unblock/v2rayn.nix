c27f91c960abfd23f13b64b72b198df00fd10424
chek1337 <DaniPlay1337@yandex.ru>
2026-05-26 00:17:57 +0700

    feat: make v2rayn to upstream

--- modules/services/unblock/v2rayn.nix
+++ modules/services/unblock/v2rayn.nix
@@ -1,7 +1,97 @@
 { inputs, ... }:
 {
   flake.modules.nixos.v2rayn =
-    { config, pkgs, ... }:
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      v2rayN-upstream = pkgs.stdenv.mkDerivation rec {
+        pname = "v2rayN";
+        version = "7.22.3";
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/2dust/v2rayN/releases/download/${version}/v2rayN-linux-64.zip";
+          hash = "sha256-QFyL+kR3S50rj77I3CY/xNLDqe82lptcSHOiqO5JRcc=";
+        };
+
+        sourceRoot = "v2rayN-linux-64";
+
+        nativeBuildInputs = with pkgs; [
+          unzip
+          autoPatchelfHook
+          copyDesktopItems
+          makeWrapper
+        ];
+
+        buildInputs = with pkgs; [
+          zlib
+          fontconfig
+          icu
+          openssl
+          krb5
+          lttng-ust_2_12
+          (lib.getLib stdenv.cc.cc)
+          libx11
+          libxrandr
+          libxi
+          libice
+          libsm
+          libxcursor
+          libxext
+        ];
+
+        # bin/xray/sing-box/mihomo/srss и geo-данные нам не нужны: HM-часть этого
+        # модуля кладёт свои xray/sing-box/geoip.dat/geosite.dat из nixpkgs в
+        # ~/.local/share/v2rayN/bin/. Удаление избавляет autoPatchelfHook от
+        # лишних Go-бинарей с непокрытыми зависимостями.
+        autoPatchelfIgnoreMissingDeps = true;
+
+        desktopItems = [
+          (pkgs.makeDesktopItem {
+            name = "v2rayn";
+            exec = "v2rayN";
+            icon = "v2rayn";
+            genericName = "v2rayN";
+            desktopName = "v2rayN";
+            categories = [ "Network" ];
+            terminal = false;
+            comment = "GUI client supporting Xray core and sing-box-core";
+          })
+        ];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/share/v2rayN
+          cp -a . $out/share/v2rayN/
+          rm -rf $out/share/v2rayN/bin
+
+          chmod +x $out/share/v2rayN/v2rayN $out/share/v2rayN/AmazTool
+
+          install -D --mode 0644 v2rayN.png \
+            $out/share/icons/hicolor/256x256/apps/v2rayn.png
+
+          mkdir -p $out/bin
+          # .NET грузит ICU через dlopen, поэтому autoPatchelfHook её не находит.
+          # Прокидываем libicu (и другие dlopen-зависимости) через LD_LIBRARY_PATH.
+          makeWrapper $out/share/v2rayN/v2rayN $out/bin/v2rayN \
+            --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath buildInputs}"
+
+          runHook postInstall
+        '';
+
+        meta = with lib; {
+          description = "GUI client supporting Xray core and sing-box-core (upstream binary)";
+          homepage = "https://github.com/2dust/v2rayN";
+          license = licenses.gpl3Plus;
+          platforms = [ "x86_64-linux" ];
+          mainProgram = "v2rayN";
+        };
+      };
+    in
     {
       sops.secrets."vless-chumakov" = {
         sopsFile = inputs.self + "/secrets/secrets.yaml";
@@ -9,7 +99,12 @@
         owner = config.settings.username;
       };
 
-      environment.systemPackages = [ pkgs.v2rayn ];
+      # v2rayN запекает /bin/bash в embedded-скриптах режима системного proxy
+      # (proxy_set_linux_sh, kill_as_sudo_linux_sh). envfs FUSE-маунтит /bin и
+      # /usr/bin поверх PATH, благодаря чему /bin/bash начинает существовать.
+      services.envfs.enable = true;
+
+      environment.systemPackages = [ v2rayN-upstream ];
     };
 
   flake.modules.homeManager.v2rayn =

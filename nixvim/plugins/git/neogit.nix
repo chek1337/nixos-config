1bf1f47e4bc6c11652aab031946868f9ce185c9e
chek1337 <DaniPlay1337@yandex.ru>
2026-05-31 15:12:34 +0700

    feat(nixvim): add <leader>g* direct popup bindings for neogit

--- nixvim/plugins/git/neogit.nix
+++ nixvim/plugins/git/neogit.nix
@@ -15,6 +15,60 @@
       action = "<cmd>Neogit<cr>";
       options.desc = "Neogit";
     }
+    {
+      key = "<leader>gc";
+      mode = "n";
+      action = "<cmd>Neogit commit<cr>";
+      options.desc = "Commit popup";
+    }
+    {
+      key = "<leader>gp";
+      mode = "n";
+      action = "<cmd>Neogit push<cr>";
+      options.desc = "Push popup";
+    }
+    {
+      key = "<leader>gP";
+      mode = "n";
+      action = "<cmd>Neogit pull<cr>";
+      options.desc = "Pull popup";
+    }
+    {
+      key = "<leader>gf";
+      mode = "n";
+      action = "<cmd>Neogit fetch<cr>";
+      options.desc = "Fetch popup";
+    }
+    {
+      key = "<leader>gl";
+      mode = "n";
+      action = "<cmd>Neogit log<cr>";
+      options.desc = "Log popup";
+    }
+    {
+      key = "<leader>gb";
+      mode = "n";
+      action = "<cmd>Neogit branch<cr>";
+      options.desc = "Branch popup";
+    }
+    {
+      key = "<leader>gs";
+      mode = "n";
+      action = "<cmd>Neogit stash<cr>";
+      options.desc = "Stash popup";
+    }
+    {
+      key = "<leader>gm";
+      mode = "n";
+      action = "<cmd>Neogit merge<cr>";
+      options.desc = "Merge popup";
+    }
+    {
+      key = "<leader>gr";
+      mode = "n";
+      action = "<cmd>Neogit rebase<cr>";
+      options.desc = "Rebase popup";
+    }
   ];
 
   plugins.neogit = {

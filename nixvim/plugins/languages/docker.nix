89db62d185aa8b0e9991129772ac305f0260f0a7
chek1337 <DaniPlay1337@yandex.ru>
2026-06-03 15:41:51 +0700

    feat(nixvim): add dockerfile treesitter parser and LSP

---
+++ nixvim/plugins/languages/docker.nix
@@ -0,0 +1,12 @@
+{ pkgs, config, ... }:
+{
+  plugins.lsp.servers.dockerls.enable = true;
+
+  plugins.treesitter.grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
+    dockerfile
+  ];
+
+  extraPackages = with pkgs; [
+    dockerfile-language-server-nodejs
+  ];
+}

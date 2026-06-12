5a4c82dca462efeb40df5be549aafd8131a02ac9
chek1337 <DaniPlay1337@yandex.ru>
2026-06-13 01:11:58 +0700

    feat: fix imv mouse-scroll

--- modules/programs/gui-tools/imv.nix
+++ modules/programs/gui-tools/imv.nix
@@ -2,17 +2,44 @@
   flake.modules.homeManager.imv =
     { pkgs, ... }:
     let
+      imvWheelZoomScale = "0.1";
+      imv = pkgs.imv.overrideAttrs (old: {
+        patches = (old.patches or [ ]) ++ [
+          (pkgs.writeText "imv-wheel-zoom-scale.patch" ''
+            diff --git a/src/imv.c b/src/imv.c
+            --- a/src/imv.c
+            +++ b/src/imv.c
+            @@ -472,2 +472,2 @@
+            -        imv_viewport_zoom(imv->view, imv->current_image, IMV_ZOOM_MOUSE,
+            -            x, y, -e->data.mouse_scroll.dy);
+            +        imv_viewport_zoom(imv->view, imv->current_image, IMV_ZOOM_MOUSE,
+            +            x, y, -e->data.mouse_scroll.dy * ${imvWheelZoomScale});
+            diff --git a/src/viewport.c b/src/viewport.c
+            --- a/src/viewport.c
+            +++ b/src/viewport.c
+            @@ -186,1 +186,1 @@
+            -                       enum imv_zoom_source src, int mouse_x, int mouse_y, int amount)
+            +                       enum imv_zoom_source src, int mouse_x, int mouse_y, double amount)
+            diff --git a/src/viewport.h b/src/viewport.h
+            --- a/src/viewport.h
+            +++ b/src/viewport.h
+            @@ -61,1 +61,1 @@
+            -                       enum imv_zoom_source, int mouse_x, int mouse_y, int amount);
+            +                       enum imv_zoom_source, int mouse_x, int mouse_y, double amount);
+          '')
+        ];
+      });
       imvSvg = pkgs.writeShellScriptBin "imv-svg" ''
         if [ "$#" -eq 1 ]; then
-          exec ${pkgs.imv}/bin/imv -b ffffff -n "$1" "$(${pkgs.coreutils}/bin/dirname "$1")"
+          exec ${imv}/bin/imv -b ffffff -n "$1" "$(${pkgs.coreutils}/bin/dirname "$1")"
         fi
 
-        exec ${pkgs.imv}/bin/imv -b ffffff "$@"
+        exec ${imv}/bin/imv -b ffffff "$@"
       '';
     in
     {
       home.packages = [
-        pkgs.imv
+        imv
         imvSvg
       ];
 

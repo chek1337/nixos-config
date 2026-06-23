7418494459d1ea7c2377f28169b0ec28e60a71ca
chek1337 <DaniPlay1337@yandex.ru>
2026-06-23 16:03:15 +0700

    feat: fix html in xml

--- nixvim/plugins/languages/xml.nix
+++ nixvim/plugins/languages/xml.nix
@@ -5,4 +5,12 @@
   plugins.treesitter.grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
     xml
   ];
+
+  # friendly-snippets не содержит xml.json (только html.json), поэтому
+  # переиспользуем HTML-сниппеты в xml-файлах: blink-источник `snippets`
+  # подтянет html.json для filetype `xml`. Триггеры вроде `div`, `a:link`
+  # станут доступны в .xml.
+  plugins.blink-cmp.settings.sources.providers.snippets.opts.extended_filetypes.xml = [
+    "html"
+  ];
 }

161b2dff10ad9f6dc1e91bc59133b7bf6fe072c3
chek1337 <DaniPlay1337@yandex.ru>
2026-06-23 13:45:00 +0700

    feat(boot): fix underlaying text

--- modules/nixos-params/nixos-classes/boot.nix
+++ modules/nixos-params/nixos-classes/boot.nix
@@ -8,6 +8,18 @@
       stylix.targets.grub.enable = false;
       boot = {
         kernelPackages = pkgs.linuxPackages_latest;
+
+        # Тихая загрузка: глушим консольный лог ядра/initrd/udev, чтобы поздние
+        # сообщения не сыпались на VT поверх tuigreet и не ломали его вывод.
+        consoleLogLevel = 0;
+        initrd.verbose = false;
+        kernelParams = [
+          "quiet"
+          "udev.log_level=3"
+          "rd.udev.log_level=3"
+          "vt.global_cursor_default=0"
+        ];
+
         loader = {
           grub = {
             enable = true;
--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -1,12 +1,15 @@
 {
   flake.modules.nixos.greetd =
-    { config, ... }:
+    { pkgs, ... }:
     {
+      # Логин-экран при загрузке: tuigreet спрашивает пароль (PAM),
+      # и только после аутентификации запускает niri-session от имени
+      # вошедшего пользователя. Сам greeter крутится под юзером `greeter`.
       services.greetd = {
         enable = true;
         settings.default_session = {
-          command = "niri-session";
-          user = config.settings.username;
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --asterisks --cmd niri-session";
+          user = "greeter";
         };
       };
     };

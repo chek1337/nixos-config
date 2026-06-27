363d2c8af45baeb434275886de174f891cefcbb2
chek1337 <DaniPlay1337@yandex.ru>
2026-06-27 11:34:06 +0700

    fix: greetd ru locale and tuigreet rename

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -5,10 +5,12 @@
       # Логин-экран при загрузке: tuigreet спрашивает пароль (PAM),
       # и только после аутентификации запускает niri-session от имени
       # вошедшего пользователя. Сам greeter крутится под юзером `greeter`.
+      systemd.services.greetd.environment.LANG = "ru_RU.UTF-8";
+
       services.greetd = {
         enable = true;
         settings.default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --remember-session --asterisks --cmd niri-session";
+          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --asterisks --cmd niri-session";
           user = "greeter";
         };
       };

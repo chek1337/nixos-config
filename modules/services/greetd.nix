210c344fab436626491a0c7b31c714cbdcdfd818
chek1337 <DaniPlay1337@yandex.ru>
2026-06-27 11:38:57 +0700

    fix: greetd use en_US locale

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -5,12 +5,16 @@
       # Логин-экран при загрузке: tuigreet спрашивает пароль (PAM),
       # и только после аутентификации запускает niri-session от имени
       # вошедшего пользователя. Сам greeter крутится под юзером `greeter`.
-      systemd.services.greetd.environment.LANG = "ru_RU.UTF-8";
-
+      #
+      # Система живёт на ru_RU.UTF-8 (i18n.defaultLocale), и PAM-сессия
+      # greeter'а наследует этот LANG из /etc/locale.conf, перебивая любой
+      # environment.LANG у сервиса. Поэтому жёстко задаём английскую локаль
+      # прямо в команде: LC_ALL имеет высший приоритет над LANG, а LANGUAGE
+      # управляет выбором перевода в gettext.
       services.greetd = {
         enable = true;
         settings.default_session = {
-          command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --asterisks --cmd niri-session";
+          command = "env LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 LANGUAGE=en_US:en ${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --asterisks --cmd niri-session";
           user = "greeter";
         };
       };
